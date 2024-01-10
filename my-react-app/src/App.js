3e1376c458eac9c51f93490c56050236c46e81ed
Meerim <meerim15179@gmail.com>
2024-01-10 23:25:29 +0100

    Add APIs, Redux and Main page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import Login from "./login/login";
 import SignUp from "./signUp/signUp";
 import Profile from "./profile/profile";
+import MainPage from "./main/main";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      {" "}
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/main" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
